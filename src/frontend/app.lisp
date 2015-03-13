18561bb7747c86d16613adf995185eb8dfcc6cd9
Mariano Montone <marianomontone@gmail.com>
2015-03-13 18:18:11 -0300

    Account management

--- src/frontend/app.lisp
+++ src/frontend/app.lisp
@@ -43,8 +43,7 @@
 		     (if *user*
 			 (who:htm (:li 
 				   :class (and (equalp active :account) "active")
-				   (:a :href (restas:genurl 'account-handler 
-							    :name (model::username *user*))
+				   (:a :href (restas:genurl 'account-handler)
 				       (who:str (model::realname *user*)))))
 			 (progn
 			   (who:htm (:li :class (and (equalp active :login) "active")
@@ -100,6 +99,17 @@
 		(:ul (:li (:a :href "http://cldm.github.io/cldm" (who:str "CLDM")))
 		     (:li (:a :href "https://github.com/cldm/cldm-registry" (who:str "CLDM Registry"))))))))
 
+(defclass logged-user-route (routes:proxy-route) ())
+
+(defmethod restas:process-route :around ((route logged-user-route) bindings)
+  (let ((*user* (hunchentoot:session-value :user)))
+    (if *user*
+	(call-next-method)
+	hunchentoot:+http-forbidden+)))
+
+(defun @logged-user (route)
+  (make-instance 'logged-user-route :target route))
+
 (restas:define-route main ("")
   (with-frontend-common (:active :home)
     (:div :class "jumbotron text-center"
@@ -123,8 +133,100 @@
 		(:h4 (who:str "API"))
 		(:p (who:str "Publish and manage your libraries via the API and the CLDM command line tool"))))))
 
+(forms:defform account-form (:action (restas:genurl 'update-account)
+				     :method :post)
+  ((username :string :label "Username" :required-p t)
+   (email :email :label "Email" :required-p t)
+   (password :password :label "Password" :required-p t)
+   (confirm-password :password :label "Confirm password" :required-p nil)
+   (realname :string :label "Realname" :required-p nil)
+   (submit :submit :label "Update")))
+
+(defun render-account-page (form)
+  (with-frontend-common ()
+    (:h1 (who:str "Your account"))
+    (forms:with-form-renderer :who
+      (let ((forms.who::*html* *html*))
+	(forms:with-form form
+	  (forms:render-form-errors)
+	  (forms:render-form-start)
+	  (with-html
+	    (:div :class "form-group"
+		  (forms:render-field-label 'username form :for "username")
+		  (forms:render-field-widget 'username form 
+					     :class "form-control col-lg-1"
+					     :placeholder "Enter username"))
+	    (:div :class "form-group"
+		  (forms::render-field-label 'email form :for "email")
+		  (forms:render-field-widget 'email form 
+					     :class "form-control col-lg-1"
+					     :placeholder "Enter an email"))
+	    (:div :class "form-group"
+		  (forms::render-field-label 'password form :for "password")
+		  (forms:render-field-widget 'password form
+					     :class "form-control col-lg-1" 
+					     :placeholader "Enter password"))
+	    (:div :class "form-group"
+		  (forms::render-field-label 'confirm-password form
+					     :for "confirm password")
+		  (forms:render-field-widget 'confirm-password form
+					     :class "form-control col-lg-1" 
+					     :placeholader "Confirm password"))
+
+	    (:div :class "form-group"
+		  (forms::render-field-label 'realname form :for "realname")
+		  (forms:render-field-widget 'realname form 
+					     :class "form-control col-lg-1"
+					     :placeholder "Enter realname"))
+	    (:div :class "form-group"
+		  (forms:render-field-widget 'submit)))
+	  (forms:render-form-end))))
+    (:h1 "API token")
+    (:p (:pre (:code (who:str (model::api-token *user*)))))))
+
 (restas:define-route account-handler ("/account")
-  )
+  (:decorators '@logged-user)
+  (let ((form (forms:get-form 'account-form)))
+    (forms:with-form-fields (username password email realname)
+	form
+	(setf (forms:field-value username) (model::username *user*))
+	(setf (forms:field-value password) (model::password *user*))
+	(setf (forms:field-value email) (model::email *user*))
+	(setf (forms:field-value realname) (model::realname *user*)))
+    (render-account-page form)))
+
+(restas:define-route update-account ("/account" :method :post)
+  (:decorators '@logged-user)
+  (let ((form (forms:get-form 'account-form)))
+    (forms:with-form form
+      (forms:handle-request)
+      (if (not (forms:validate-form))
+	  (render-account-page form)
+					; else
+	  (forms:with-form-field-values (username password confirm-password email realname)
+	      form
+	    (cond
+	      ((and (string-not-equal username (model::username *user*))
+		    (model::find-user-by-username username))
+	       (push (cons (forms::get-field form 'username)
+			   (list "Username already taken. Choose another."))
+		     (forms::form-errors form))
+	       (render-account-page form))
+	      ((and (string-not-equal (model::password *user*)
+				      password)
+		    (not (string= password confirm-password)))
+	       (push (cons (forms::get-field form 'confirm-password)
+			   (list "Passwords don't match"))
+		     (forms::form-errors form))
+	       (render-account-page form))
+	      ;; else, valid form, update the user account
+	      (t
+	       (setf (model::username *user*) username)
+	       (setf (model::password *user*) password)
+	       (setf (model::email *user*) email)
+	       (setf (model::realname *user*) realname)
+	       (model::store *user*)
+	       (restas:redirect 'account-handler))))))))
 
 (restas:define-route logout ("/logout")
   (setf (hunchentoot:session-value :user) nil)
