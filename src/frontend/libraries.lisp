b0dad5b3f341d33e2881aa00481475febc8d2b46
Mariano Montone <marianomontone@gmail.com>
2015-03-14 15:45:46 -0300

    Library version page

--- src/frontend/libraries.lisp
+++ src/frontend/libraries.lisp
@@ -141,9 +141,54 @@
   (let ((library-version (model::find-library-version name version)))
     (if (not library-version)
         hunchentoot:+http-not-found+
-        (with-frontend-common (:active :browse)
-          (:h1 (who:str (print-library-version-to-string library-version)))
-          (:p (who:str (model::description library-version)))))))
+	(let ((library (model::library library-version)))
+	  (with-frontend-common (:active :browse)
+	    (:div :class "row"
+		  (:h1 (who:str (print-library-version-to-string library-version)))
+		  (:p (who:str (model::description library-version)))
+		  (:p (who:str (model::description library)))
+		  (:p (:b (who:str "Author: ")) (who:fmt "~A" (model::author library)))
+		  (:p (:b (who:str "Categories: "))
+		      (let ((categories (model::categories library)))
+			(loop for category in (butlast categories)
+			   do
+			     (who:htm (:a :href (restas:genurl 'category-handler :name category)
+					  (who:str category))
+				      (who:str ", ")))
+			(let ((category (car (last categories))))
+			  (who:htm (:a :href (restas:genurl 'category-handler :name category)
+				       (who:str category))))))
+		  (:p (:b (who:str "Keywords: "))
+		      (let ((keywords (model::keywords library)))
+			(loop for keyword in (butlast keywords)
+			   do
+			     (who:htm (:a :href (restas:genurl 'keyword-handler :name keyword)
+					  (who:str keyword))
+				      (who:str ", ")))
+			(let ((keyword (car (last keywords))))
+			  (who:htm (:a :href (restas:genurl 'keyword-handler :name keyword)
+				       (who:str keyword))))))
+		  (:p (:b (who:str "Licence: ")) (who:fmt "~A" (model::licence library)))
+		  (:p (:b (who:str "Version: "))
+		      (who:str (semver:print-version-to-string 
+				(model::version library-version))))
+		  (:p (:b (who:str "Dependencies: "))
+		      (let ((deps (model::dependencies library-version)))
+			(loop for dep in (butlast deps)
+			   do
+			     (who:htm (render-requirement dep)
+				      (who:str ", ")))
+			(let ((dep (car (last deps))))
+			  (render-requirement dep))))
+		  (let ((publisher (model::publisher library)))
+		    (who:htm
+		     (:p (:b (who:str "Publisher: "))
+			 (:a :href (restas:genurl 'user-handler 
+						  :username 
+						  (model::username publisher))
+			     (who:str (print-user-name publisher))))))
+		  (:p (:b (who:str "Published: "))
+		      (who:str (mongo::fmt (model::creation-time library-version) nil)))))))))
 
 (restas:define-route library-cld-handler ("/libraries/:(name).cld")
   (let ((library (model::find-library-by-name name)))
